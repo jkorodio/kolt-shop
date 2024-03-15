97503b2e4da5b9b39c51227cfea3d5b695660962
Jeremy Keith Y. Orodio <jyorodio@rbssoftware.solutions.xyz>
2024-03-15 21:18:29 +0800

    add frontend authentication

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,10 @@
-import React from 'react'
-import { Container } from 'react-bootstrap'
-import { Outlet } from 'react-router-dom'
-import Footer from './components/Footer'
-import Header from './components/Header'
+import React from 'react';
+import { Container } from 'react-bootstrap';
+import { Outlet } from 'react-router-dom';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
   return (
@@ -14,6 +16,7 @@ const App = () => {
         </Container>
       </main>
       <Footer />
+      <ToastContainer />
     </>
   )
 }
