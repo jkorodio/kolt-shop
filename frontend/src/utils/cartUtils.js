601efe71d7ab6a8bc10e1ea529fbf234ac901c59
Jeremy Keith Y. Orodio <jyorodio@rbssoftware.solutions.xyz>
2024-04-19 09:03:28 +0800

    update cart utils

--- frontend/src/utils/cartUtils.js
+++ frontend/src/utils/cartUtils.js
@@ -1,22 +1,36 @@
 
 export const addDecimals = (num) => {
-    return (Math.round(num * 100) / 100).toFixed(2)
-}
+    return (Math.round(num * 100) / 100).toFixed(2);
+};
+
+// NOTE: the code below has been changed from the course code to fix an issue
+// with type coercion of strings to numbers.
+// Our addDecimals function expects a number and returns a string, so it is not
+// correct to call it passing a string as the argument.
 
 export const updateCart = (state) => {
-    // calculate items price
-    state.itemsPrice = addDecimals(state.cartItems.reduce((acc, item) => acc + item.price * item.qty, 0));
+    // Calculate the items price in whole number (pennies) to avoid issues with
+    // floating point number calculations
+    const itemsPrice = state.cartItems.reduce(
+        (acc, item) => acc + (item.price * 100 * item.qty) / 100,
+        0
+    );
+    state.itemsPrice = addDecimals(itemsPrice);
 
-    // calculate shipping price (if order is over 1000 then free, 150 shipping)
-    state.shippingPrice = addDecimals(state.itemsPrice > 1000 ? 0 : 150);
+    // Calculate the shipping price
+    const shippingPrice = itemsPrice > 100 ? 0 : 10;
+    state.shippingPrice = addDecimals(shippingPrice);
 
-    // calculate tax price (15% tax)
-    state.taxPrice = addDecimals(Number((0.15 * state.itemsPrice).toFixed(2)));
+    // Calculate the tax price
+    const taxPrice = 0.15 * itemsPrice;
+    state.taxPrice = addDecimals(taxPrice);
 
-    // calculate total price
-    state.totalPrice = (Number(state.itemsPrice) + Number(state.shippingPrice) + Number(state.taxPrice)).toFixed(2);
+    const totalPrice = itemsPrice + shippingPrice + taxPrice;
+    // Calculate the total price
+    state.totalPrice = addDecimals(totalPrice);
 
+    // Save the cart to localStorage
     localStorage.setItem('cart', JSON.stringify(state));
 
-    return state
-}
+    return state;
+};
