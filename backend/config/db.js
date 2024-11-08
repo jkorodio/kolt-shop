64e5ea16f6eea4b32a7abd62fa2fb77cde793cc1
Jeremy Kieth Orodio <82317503+jkorodio@users.noreply.github.com>
2024-11-08 11:47:16 +0800

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,7 +5,7 @@ const connectDB = async () => {
         const conn = await mongoose.connect(process.env.MONGO_URI);
         console.log(`MongoDB connected: ${conn.connection.host}`);
     } catch (error) {
-        console.log(`Error: ${error.message}`); // Use `error.message` here
+        console.log(`Error: ${error.message}`);
         process.exit(1);
     }
 }
