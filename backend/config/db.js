c08ed95fb594cf4186fee131c5df4273a76f2861
Jeremy Kieth Orodio <82317503+jkorodio@users.noreply.github.com>
2024-11-08 12:04:52 +0800

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,6 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
     try {
+        console.log('Connecting to MongoDB URI:', process.env.MONGO_URI); // For debugging
         const conn = await mongoose.connect(process.env.MONGO_URI);
         console.log(`MongoDB connected: ${conn.connection.host}`);
     } catch (error) {
